0e95e5a38557513aba39c24f8cb4e7bba76fee2f
byetiskin <begum.yetiskin@inonu.edu.tr>
2024-10-07 00:55:50 +0300

    Add files via upload

---
+++ Hafta2/Hafta2_1.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+# Görüntüyü açma
+image_path = 'C:/Users/Dell/Downloads/Asistanlık/Dersler/Görüntü İşleme/Uygulamalar-Yeni/Hafta 2/galaksi.jpg'
+image = Image.open(image_path)
+image = np.array(image)
+
+# Gaussian gürültüsü parametreleri
+mean = 0
+sigma = 100
+gaussian_noise = np.random.normal(mean, sigma, image.shape)
+
+# Gürültüyü görüntüye ekleme
+noisy_image = image + gaussian_noise
+
+# Piksel değerlerinin 0-255 aralığında kalması için limitleme
+noisy_image = np.clip(noisy_image, 0, 255).astype(np.uint8)
+
+# Görüntüleri gösterme
+plt.subplot(1, 2, 1)
+plt.imshow(image, cmap='gray')
+plt.title('Orijinal Görüntü')
+plt.axis('off')
+
+plt.subplot(1, 2, 2)
+plt.imshow(noisy_image, cmap='gray')
+plt.title('Gaussian Gürültülü Görüntü')
+plt.axis('off')
+
+plt.show()
